3eb1ccc0334c3695890a968fd1f34965d61d1872
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2025-11-22 15:34:38 +0500

    Create intro.py

---
+++ intro.py
@@ -0,0 +1,5 @@
+# Fayllar bilan ishlash 
+# 127.0.1.1 > DNS (domain name system) > ketmon.uz
+# url > kun.uz > server > django >  index.html
+# url > kun.uz/contact > server > django > contact.html
+# kun.uz/home?post_id=1
